d0504e88494ac792661a41209649fb609884d365
Łukasz Bożek <xxlukasz11@gmail.com>
2021-12-02 06:18:31 +0100

    day 2 solution

--- day2/main.cpp
+++ day2/main.cpp
@@ -2,26 +2,67 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <sstream>
 
-using DataType = std::vector<int>;
+
+enum class Dir {
+	HORIZONTAL,
+	VERTICAL
+};
+
+struct Command {
+	Dir dir;
+	int value;
+};
+
+using DataType = std::vector<Command>;
 
 DataType read() {
 	common::FileReader reader("input.txt");
 	DataType data;
 	for (std::string line; reader.nextLine(line);) {
-
+		std::istringstream ss(line);
+		std::string dir;
+		ss >> dir;
+		int value;
+		ss >> value;
+		if (dir == "forward") {
+			data.push_back({ Dir::HORIZONTAL, value });
+		} else if (dir == "down") {
+			data.push_back({ Dir::VERTICAL, value });
+		} else {
+			data.push_back({ Dir::VERTICAL, -value });
+		}
 	}
 	return data;
 }
 
 void partOne(const DataType& data) {
-
-	std::cout << "Part one: " << std::endl;
+	int horizontal = 0;
+	int depth = 0;
+	for (auto [dir, value] : data) {
+		if (dir == Dir::HORIZONTAL) {
+			horizontal += value;
+		} else {
+			depth += value;
+		}
+	}
+	std::cout << "Part one: " << horizontal * depth << std::endl;
 }
 
 void partTwo(const DataType& data) {
-
-	std::cout << "Part two: " << std::endl;
+	int horizontal = 0;
+	int depth = 0;
+	int aim = 0;
+	for (auto [dir, value] : data) {
+		if (dir == Dir::HORIZONTAL) {
+			horizontal += value;
+			depth += aim * value;
+		} else {
+			aim += value;
+		}
+	}
+	std::cout << "Part two: " << horizontal * depth << std::endl;
 }
 
 int main() {
