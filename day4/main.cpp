57d3aba5378356a50cc3dbbf9067b97063b2b503
Łukasz Bożek <xxlukasz11@gmail.com>
2021-12-04 07:05:24 +0100

    solve day 4

--- day4/main.cpp
+++ day4/main.cpp
@@ -3,26 +3,169 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <sstream>
 
-using DataType = std::vector<int>;
+struct Field {
+	int val;
+	bool set = false;
+
+	Field(int arg) : val(arg) {}
+};
+
+struct Board {
+	int id;
+	std::vector<std::vector<Field>> array;
+};
+
+struct InputData {
+	std::vector<int> numbers;
+	std::vector<Board> boards;
+};
+
+using DataType = InputData;
 
 DataType read() {
 	common::FileReader reader("input.txt");
 	DataType data;
-	for (std::string line; reader.nextLine(line);) {
 
+	std::string numbers = reader.nextLine();
+	std::istringstream ss(numbers);
+	for (int val; ss >> val;) {
+		data.numbers.push_back(val);
+		if (ss.peek() == ',') {
+			ss.ignore();
+		}
+	}
+
+	int boardId = 1;
+	for (std::string line; reader.nextLine(line);) {
+		if (line.empty()) {
+			data.boards.push_back(Board{});
+			data.boards.back().id = boardId++;
+			continue;
+		}
+
+		std::istringstream str(line);
+		std::vector<Field> row;
+		for (int val; str >> val;) {
+			row.push_back(val);
+		}
+		data.boards.back().array.push_back(row);
 	}
 	return data;
 }
 
-void partOne(const DataType& data) {
+void markFields(Board& board, int value) {
+	for (auto& row : board.array) {
+		for (auto& [num, set] : row) {
+			if (num == value) {
+				set = true;
+			}
+		}
+	}
+}
 
-	std::cout << "Part one: " << std::endl;
+bool isWinning(const Board& board) {
+	// check rows
+	for (const auto& row : board.array) {
+		int setCount = 0;
+		for (const auto& [_, set] : row) {
+			if (set) {
+				++setCount;
+			}
+		}
+		if (setCount == row.size()) {
+			return true;
+		}
+	}
+
+	// check columns
+	int rows = board.array.size();
+	int cols = board.array[0].size();
+	for (int col = 0; col < cols; ++col) {
+		int setCount = 0;
+		for (const auto& row : board.array) {
+			if (row[col].set) {
+				++setCount;
+			}
+		}
+		if (setCount == cols) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+std::pair<Board, int> getWinningBoard(const DataType& data) {
+	auto [numbers, boards] = data;
+	for (int currentNumber : numbers) {
+		
+		for (Board& board : boards) {
+			markFields(board, currentNumber);
+			if (isWinning(board)) {
+				return {board, currentNumber};
+			}
+		}
+
+	}
+	throw "So bad";
+}
+
+std::pair<Board, int> getLosingBoard(const DataType& data) {
+	auto [numbers, boards] = data;
+	Board* losingBoard = nullptr;
+	for (int currentNumber : numbers) {
+
+		int winningBoardCount = 0;
+		for (Board& board : boards) {
+			markFields(board, currentNumber);
+			if (isWinning(board)) {
+				++winningBoardCount;
+			}
+		}
+
+		int notWinningBoardCount = boards.size() - winningBoardCount;
+		if (notWinningBoardCount == 0) {
+			if (!losingBoard) {
+				throw "So bad";
+			}
+			return { *losingBoard, currentNumber };
+		}
+		else if (notWinningBoardCount == 1) {
+			for (Board& board : boards) {
+				if (!isWinning(board)) {
+					losingBoard = &board;
+				}
+			}
+		}
+	}
+
+	throw "So bad";
+}
+
+int sumUnsetFields(const Board& board) {
+	int sum = 0;
+	for (const auto& row : board.array) {
+		for (auto [val, set] : row) {
+			if (!set) {
+				sum += val;
+			}
+		}
+	}
+	return sum;
+}
+
+void partOne(const DataType& data) {
+	auto [winningBoard, lastNumber] = getWinningBoard(data);
+	int sum = sumUnsetFields(winningBoard);
+	std::cout << "Part one: " << sum*lastNumber << std::endl;
 }
 
 void partTwo(const DataType& data) {
-
-	std::cout << "Part two: " << std::endl;
+	auto [losingBoard, lastNumber] = getLosingBoard(data);
+	int sum = sumUnsetFields(losingBoard);
+	std::cout << "Part two: " << sum * lastNumber << std::endl;
 }
 
 int main() {
