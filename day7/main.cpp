31c6aefe81250a97f733515867409588eecdbdbf
Łukasz Bożek <xxlukasz11@gmail.com>
2021-12-07 06:24:06 +0100

    solve day7

--- day7/main.cpp
+++ day7/main.cpp
@@ -3,26 +3,66 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
+#include <utility>
 
 using DataType = std::vector<int>;
 
 DataType read() {
 	common::FileReader reader("input.txt");
-	DataType data;
-	for (std::string line; reader.nextLine(line);) {
-
-	}
+	std::string line = reader.nextLine();
+	DataType data = common::parseArray<int>(line, ',');
 	return data;
 }
 
 void partOne(const DataType& data) {
+	int min = *std::min_element(data.begin(), data.end());
+	int max = *std::max_element(data.begin(), data.end());
 
-	std::cout << "Part one: " << std::endl;
+	std::vector<std::pair<int, int>> costs;
+	for (int dst = min; dst <= max; ++dst) {
+		int fuel = 0;
+		for (int src : data) {
+			fuel += abs(src - dst);
+		}
+		costs.emplace_back(dst, fuel);
+	}
+
+	auto minCosts = *std::min_element(costs.begin(), costs.end(), [](const auto l, const auto r) {
+		return l.second < r.second;
+	});
+
+	std::cout << "Part one: " << minCosts.first << " " << minCosts.second << std::endl;
+}
+
+int sumFuel(int src, int dst) {
+	int inc = 1;
+	int sum = 0;
+	int steps = abs(src - dst);
+	for (int i = 0; i < steps; ++i) {
+		sum += inc++;
+	}
+	return sum;
 }
 
 void partTwo(const DataType& data) {
+	int min = *std::min_element(data.begin(), data.end());
+	int max = *std::max_element(data.begin(), data.end());
 
-	std::cout << "Part two: " << std::endl;
+	std::vector<std::pair<int, int>> costs;
+	for (int dst = min; dst <= max; ++dst) {
+		int fuel = 0;
+		for (int src : data) {
+			fuel += sumFuel(src, dst);
+		}
+		costs.emplace_back(dst, fuel);
+	}
+
+	auto minCosts = *std::min_element(costs.begin(), costs.end(), [](const auto l, const auto r) {
+		return l.second < r.second;
+	});
+
+	std::cout << "Part two: " << minCosts.first << " " << minCosts.second << std::endl;
 }
 
 int main() {
