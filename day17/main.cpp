2fe1bfa314495d15ed93182b05f3df59de8786aa
Lukasz Bozek <xxlukasz11@gmail.com>
2021-12-17 07:29:58 +0100

    solve day 17

--- day17/main.cpp
+++ day17/main.cpp
@@ -1,34 +1,99 @@
 #include "../common/pch.h"
 #include <iostream>
-#include <cstdint>
 #include <string>
-#include <algorithm>
-#include <numeric>
-#include <utility>
 #include <sstream>
-#include <vector>
-#include <map>
-#include <set>
 
-using DataType = std::vector<int>;
+struct Data {
+	int minX;
+	int maxX;
+	int minY;
+	int maxY;
+};
+
+using DataType = Data;
 
 DataType read() {
 	common::FileReader reader("input.txt");
 	DataType data;
-	for (std::string line; reader.nextLine(line);) {
-
-	}
+	std::string line = reader.nextLine();
+	auto eq = line.find('=');
+	std::istringstream ss(line.substr(eq + 1));
+	ss >> data.minX;
+	ss.ignore(2);
+	ss >> data.maxX;
+	ss.ignore(4);
+	ss >> data.minY;
+	ss.ignore(2);
+	ss >> data.maxY;
 	return data;
 }
 
-int partOne(const DataType& data) {
+int findMinXSpeed(int xMin, int xMax) {
+	int initialSpeed = 0;
+	while (true) {
+		++initialSpeed;
+		int currentSpeed = initialSpeed;
+		int x = 0;
+		while (currentSpeed > 0) {
+			x += currentSpeed;
+			--currentSpeed;
+		}
+		if (x <= xMax && x >= xMin) {
+			return initialSpeed;
+		}
+	}
+	throw "So bad";
+}
 
-	return 0;
+bool checkIfHitTheTarget(const DataType& data, const int vx, const int vy) {
+	int currentXSpeed = vx;
+	int currentYSpeed = vy;
+	int y = 0;
+	int x = 0;
+	while (true) {
+		y += currentYSpeed;
+		x += currentXSpeed;
+		--currentYSpeed;
+		if (currentXSpeed > 0) {
+			--currentXSpeed;
+		}
+
+		if (x > data.maxX || y < data.minY) {
+			return false;
+		}
+		else if (x >= data.minX && y <= data.maxY) {
+			return true;
+		}
+	}
+}
+
+int partOne(const DataType& data) {
+	int speedY = -data.minY - 1;
+	int y = 0;
+	int maxY = 0;
+	while (y > data.maxY) {
+		y += speedY;
+		--speedY;
+		if (maxY < y) {
+			maxY = y;
+		}
+	}
+	return maxY;
 }
 
 int partTwo(const DataType& data) {
+	const int vyMax = -data.minY - 1;
+	const int vyMin = data.minY;
+	const int vxMin = findMinXSpeed(data.minX, data.maxX);
+	const int vxMax = data.maxX;
 
-	return 0;
+	int noOfTrajectories = 0;
+	for (int vx = vxMin; vx <= vxMax; ++vx) {
+		for (int vy = vyMin; vy <= vyMax; ++vy) {
+			noOfTrajectories += checkIfHitTheTarget(data, vx, vy);
+		}
+	}
+	return noOfTrajectories;
 }
 
 int main(int argc, char** argv) {
